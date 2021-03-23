fb8de99c599241e4896aac1921b074d816ec1f40
anttipeltola <antti.peltola@gofore.com>
2021-03-23 08:57:34 +0200

    TREV-57 customize logo, footer cityLabel, urgencyAttachments

--- frontend/tampere/citizen.tsx
+++ frontend/tampere/citizen.tsx
@@ -1,18 +1,19 @@
 import { CitizenCustomizations } from '@evaka/lib-customizations/types'
+import enCustomizations from './enCustomizations'
+import fiCustomizations from './fiCustomizations'
+import svCustomizations from './svCustomizations'
 import TampereLogo from './city-logo.svg'
-import enCustomizations from './en-customizations'
-import fiCustomizations from './fi-customizations'
-import svCustomizations from './sv-customizations'
-
+import featureFlags from './featureFlags'
 
 const customizations: CitizenCustomizations = {
+  fiCustomizations,
+  svCustomizations,
+  enCustomizations,
   cityLogo: {
     src: TampereLogo,
     alt: 'Tampere logo'
   },
-  fiCustomizations,
-  svCustomizations,
-  enCustomizations
+  featureFlags
 }
 
 export default customizations
--- frontend/tampere/employee.tsx
+++ frontend/tampere/employee.tsx
@@ -1,11 +1,13 @@
 import { EmployeeCustomizations } from '@evaka/lib-customizations/types'
 import TampereLogo from './city-logo.svg'
+import featureFlags from './featureFlags'
 
 const customizations: EmployeeCustomizations = {
   cityLogo: {
     src: TampereLogo,
     alt: 'Tampere logo'
-  }
+  },
+  featureFlags
 }
 
 export default customizations
