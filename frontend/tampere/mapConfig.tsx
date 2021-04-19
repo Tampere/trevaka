bba77e982ca54909dde0105924bdb8a45ef6cc03
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-04-19 09:03:48 +0300

    Add map config

---
+++ frontend/tampere/mapConfig.tsx
@@ -0,0 +1,9 @@
+import { MapConfig } from 'lib-customizations/types'
+
+const mapConfig: MapConfig = {
+  center: [61.49836, 23.76036],
+  initialZoom: 11,
+  addressZoom: 14
+}
+
+export default mapConfig
