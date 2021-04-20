2a01d528d2467916d6357bb698be447593dd13f1
Tatu Repo <tatu.repo@gofore.com>
2021-04-20 15:41:58 +0300

    Added a pm2 config for running trevaka apps and a compose for running just dbs on the trevaka side

---
+++ compose/ecosystem.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+const defaults = {
+  autorestart: false
+}
+
+module.exports = {
+  apps: [{
+    name: 'apigw',
+    script: 'yarn && yarn clean && yarn dev',
+    cwd: path.resolve(__dirname, '../evaka/apigw'),
+    ...defaults
+  }, {
+    name: 'frontend',
+    script: 'yarn && yarn clean && EVAKA_CUSTOMIZATIONS=tampere yarn dev',
+    cwd: path.resolve(__dirname, '../evaka/frontend'),
+    env: {
+      'ICONS': process.env.ICONS
+    },
+    ...defaults
+  }, {
+    name: 'service',
+    script: `${__dirname}/../evaka/compose/run-after-db.sh`,
+    args: './gradlew --no-daemon bootRun',
+    cwd: path.resolve(__dirname, '../service'),
+    ...defaults
+  },
+    /*{
+    name: 'message-srv',
+    script: `${__dirname}/run-after-db.sh`,
+    args: './gradlew --no-daemon bootRun',
+    cwd: path.resolve(__dirname, '../message-service'),
+    ...defaults
+  }*/
+  ],
+}
