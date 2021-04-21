6c5694e78f8c7ced8ad61f8d5fed33499159b23e
anttipeltola <antti.peltola@gofore.com>
2021-04-21 16:55:07 +0300

    TREV-146 customize application list view texts

--- frontend/tampere/enCustomizations.tsx
+++ frontend/tampere/enCustomizations.tsx
@@ -1,10 +1,16 @@
-{/*
+{
+  /*
 SPDX-FileCopyrightText: 2021 City of Tampere
 
 SPDX-License-Identifier: LGPL-2.1-or-later
-*/}
+*/
+}
 
 export default {
+  applicationsList: {
+    title: 'TODO (en) Hakeminen varhaiskasvatukseen',
+    summary: `TODO (en) Lapsen huoltaja voi tehdä lapselleen hakemuksen varhaiskasvatukseen ja kerhoon. Huoltajan lasten tiedot haetaan tähän näkymään automaattisesti Väestötietojärjestelmästä.`
+  },
   footer: {
     cityLabel: '© City of Tampere',
     privacyPolicyLink: 'TODO',
