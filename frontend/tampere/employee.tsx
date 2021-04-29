da81f2d826f8a10a19bbdb3d69a37a50311918f2
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-04-29 11:29:05 +0300

    Revert "Add employee translations"
    This reverts commit d3bdf945dae8f6c8bdb7fe184ac4dc2051feb245.

--- frontend/tampere/employee.tsx
+++ frontend/tampere/employee.tsx
@@ -7,14 +7,10 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 }
 
 import { EmployeeCustomizations } from 'lib-customizations/types'
-import { fi } from './employee/assets/i18n/fi'
 import TampereLogo from './city-logo.svg'
 import featureFlags from './featureFlags'
 
 const customizations: EmployeeCustomizations = {
-  translations: {
-    fi
-  },
   cityLogo: {
     src: TampereLogo,
     alt: 'Tampere logo'
