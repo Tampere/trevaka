05232f4bed7df055456cea3a0a8ae80ed4a75b34
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-05-04 12:02:46 +0300

    Add club application text overrides

--- frontend/tampere/fiCustomizations.tsx
+++ frontend/tampere/fiCustomizations.tsx
@@ -12,6 +12,70 @@ import { DeepPartial } from 'lib-customizations/types'
 import React from 'react'
 
 const fi: DeepPartial<Translations> = {
+  applications: {
+    editor: {
+      heading: {
+        info: {
+          CLUB: function EditorHeadingInfoClubText() {
+            return (
+              <>
+                <P>
+                  Kerhopaikkaa voi hakea ympäri vuoden. Kerhohakemuksella voi
+                  hakea kunnallista tai palvelusetelillä tuettua kerhopaikkaa.
+                  Kirjallinen ilmoitus kerhopaikasta lähetetään huoltajalle
+                  Suomi.fi-viestit-palveluun. Mikäli huoltaja ei ole ottanut
+                  Suomi.fi-viestit- palvelua käyttöön, ilmoitus lähetetään
+                  hänelle postitse. Paikka myönnetään yhdeksi toimintakaudeksi
+                  kerrallaan.
+                </P>
+                <P>
+                  Kerhohakemus kohdistuu yhdelle kerhon toimintakaudelle.
+                  Kyseisen kauden päättyessä hakemus poistetaan järjestelmästä.
+                </P>
+              </>
+            )
+          }
+        }
+      },
+      serviceNeed: {
+        clubDetails: {
+          wasOnDaycareInfo:
+            'Jos lapsi on ollut kunnallisessa päiväkodissa tai perhepäivähoidossa ja hän luopuu paikastaan kerhon alkaessa, hänellä on suurempi mahdollisuus saada kerhopaikka.',
+          wasOnClubCareInfo:
+            'Jos lapsi on ollut kerhossa jo edellisen toimintakauden aikana, hänellä on suurempi mahdollisuus saada paikka kerhosta myös tulevana toimintakautena.'
+        },
+        assistanceNeedInstructions:
+          'Jos lapsella on tuen tarve, Tampereen varhaiskasvatuksesta otetaan yhteyttä hakemuksen jättämisen jälkeen.'
+      },
+      contactInfo: {
+        info: function ContactInfoInfoText() {
+          return (
+            <P>
+              Henkilötiedot on haettu väestötiedoista, eikä niitä voi muuttaa
+              tällä hakemuksella. Jos henkilötiedoissa on virheitä, päivitäthän
+              tiedot{' '}
+              <a
+                href="https://dvv.fi/henkiloasiakkaat"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Digi- ja Väestötietoviraston sivuilla
+              </a>
+              . Mikäli osoitteenne on muuttumassa, voit lisätä tulevan osoitteen
+              erilliseen kohtaan hakemuksella; lisää tuleva osoite sekä lapselle
+              että huoltajalle. Virallisena osoitetietoa pidetään vasta, kun se
+              on päivittynyt väestötietojärjestelmään. Varhaiskasvatus- ja
+              palvelusetelipäätös sekä tieto avoimen varhaiskasvatuksen
+              kerhopaikasta toimitetaan automaattisesti myös eri osoitteessa
+              asuvalle väestötiedoista löytyvälle huoltajalle.
+            </P>
+          )
+        },
+        futureAddressInfo:
+          'Tampereen varhaiskasvatuksessa virallisena osoitteena pidetään väestötiedoista saatavaa osoitetta. Osoite väestötiedoissa muuttuu hakijan tehdessä muuttoilmoituksen postiin tai maistraattiin.'
+      }
+    }
+  },
   applicationsList: {
     title: 'Hakeminen varhaiskasvatukseen',
     summary: function ApplicationListSummaryText() {
