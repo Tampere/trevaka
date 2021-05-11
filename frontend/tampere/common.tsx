64e69c0f22a3e0a0acfec7bb19a78dc9c873fc22
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-05-11 13:58:19 +0300

    Add tampere theme

---
+++ frontend/tampere/common.tsx
@@ -0,0 +1,8 @@
+import { CommonCustomizations } from 'lib-customizations/types'
+import theme from './tampere-theme'
+
+const customizations: CommonCustomizations = {
+  theme
+}
+
+export default customizations
---
+++ frontend/tampere/tampere-theme.tsx
@@ -0,0 +1,42 @@
+import { Theme } from 'lib-common/theme'
+
+const theme: Theme = {
+  colors: {
+    brand: {
+      primary: '#00417D',
+      secondary: '#39A7D7',
+      secondaryLight: '#e9f5ff'
+    },
+    main: {
+      dark: '#00417D',
+      medium: '#4676BE',
+      primary: '#29549A',
+      primaryHover: '#4676BE',
+      primaryActive: '#00417D',
+      light: '#C6D7E3',
+      lighter: '#E5EEF8'
+    },
+    greyscale: {
+      darkest: '#0F0F0F',
+      dark: '#585857',
+      medium: '#B1B1B1',
+      lighter: '#D8D8D8',
+      lightest: '#F5F5F5',
+      white: '#FFFFFF'
+    },
+    accents: {
+      orange: '#FF7300',
+      orangeDark: '#B85300',
+      green: '#ABC872',
+      greenDark: '#4C7A00',
+      water: '#91C9EA',
+      yellow: '#F4D240',
+      red: '#CB181A',
+      petrol: '#5390B5',
+      emerald: '#397368',
+      violet: '#7361A2'
+    }
+  }
+}
+
+export default theme
