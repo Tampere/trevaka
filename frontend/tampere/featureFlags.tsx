977b14c4bc8fb739a17859d2e50f1d11afa1380f
anttipeltola <antti.peltola@gofore.com>
2021-05-19 10:09:31 +0300

    TREV-165 enable daycare application service need options + update evaka submodule

--- frontend/tampere/featureFlags.tsx
+++ frontend/tampere/featureFlags.tsx
@@ -1,5 +1,3 @@
-import { FeatureFlags } from 'lib-customizations/types'
-
 {
   /*
 SPDX-FileCopyrightText: 2021 City of Tampere
@@ -8,8 +6,13 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 */
 }
 
+import { FeatureFlags } from 'lib-customizations/types'
+
 const featureFlags: FeatureFlags = {
-  daycareApplicationServiceNeedOptionsEnabled: true,
+  daycareApplication: {
+    serviceNeedOptionsEnabled: true,
+    dailyTimesEnabled: false
+  },
   urgencyAttachmentsEnabled: false,
   preschoolEnabled: false,
   assistanceActionOtherEnabled: false
