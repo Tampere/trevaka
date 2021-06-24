71e6a5c8dbf34bc4e1d1036061d4a5a6bf2f06a2
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-06-24 14:36:16 +0300

    Update evaka submodule

--- service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
+++ service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 object TrevakaServiceDeps {
-    const val kotlin = "1.4.21"
-    const val springBoot = "2.4.1"
+    const val kotlin = "1.5.10"
+    const val springBoot = "2.5.1"
     const val springCloud = "2020.0.2"
     const val java = "11"
     const val xjc = "2.0.0"
