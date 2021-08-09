ad5d5b51e749473eacdcd115f73027288e9509a4
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-08-09 08:30:16 +0300

    Update evaka submodule

--- service/src/main/kotlin/fi/tampere/trevaka/scheduler/config/SchedulerConfiguration.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/scheduler/config/SchedulerConfiguration.kt
@@ -1,15 +1,15 @@
 package fi.tampere.trevaka.scheduler.config
 
+import fi.espoo.evaka.ScheduledJobsEnv
 import fi.espoo.evaka.shared.job.DefaultJobSchedule
 import fi.espoo.evaka.shared.job.JobSchedule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 @Configuration
 class SchedulerConfiguration {
 
     @Bean
-    fun jobSchedule(env: Environment): JobSchedule = DefaultJobSchedule.fromEnvironment(env)
+    fun jobSchedule(env: ScheduledJobsEnv): JobSchedule = DefaultJobSchedule(env)
 
 }
