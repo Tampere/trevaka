b867e40f2a0d28db648dad4fa7d23af5058cfb1c
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-09-07 14:22:35 +0300

    Add income types

--- service/src/main/kotlin/fi/tampere/trevaka/invoice/config/InvoiceConfiguration.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/invoice/config/InvoiceConfiguration.kt
@@ -4,7 +4,9 @@
 
 package fi.tampere.trevaka.invoice.config
 
+import fi.espoo.evaka.invoicing.domain.IncomeType
 import fi.espoo.evaka.invoicing.integration.InvoiceIntegrationClient
+import fi.espoo.evaka.invoicing.service.IncomeTypesProvider
 import fi.tampere.trevaka.TrevakaProperties
 import fi.tampere.trevaka.invoice.service.TrevakaInvoiceClient
 import fi.tampere.trevaka.util.basicAuthInterceptor
@@ -69,4 +71,31 @@ class InvoiceConfiguration {
         .addInterceptorFirst(basicAuthInterceptor(properties.ipaas.username, properties.ipaas.password))
         .build()
 
+    @Bean
+    fun incomeTypesProvider(): IncomeTypesProvider = TampereIncomeTypesProvider()
+
+}
+
+class TampereIncomeTypesProvider : IncomeTypesProvider {
+    override fun get(): Map<String, IncomeType> {
+        return linkedMapOf(
+            "PENSION" to IncomeType("Eläke", 1, false, false),
+            "MAIN_INCOME" to IncomeType("Palkkatulo", 1, true, false),
+            "HOLIDAY_BONUS" to IncomeType("Lomaraha", 1, false, false),
+            "ALIMONY" to IncomeType("Saadut elatusavut", 1, false, false),
+            "PAID_ALIMONY" to IncomeType("Maksetut elatusavut", -1, false, false),
+            "OTHER_INCOME" to IncomeType("Muu tulo", 1, true, false),
+            "DAILY_ALLOWANCE" to IncomeType("Päiväraha", 1, false, false),
+            "BUSINESS_INCOME" to IncomeType("Yritystoiminnan tulo", 1, false, false),
+            "ADJUSTED_DAILY_ALLOWANCE" to IncomeType("Soviteltu päiväraha", 1, false, false),
+            "PERKS" to IncomeType("Luontaisetu", 1, false, false),
+            "HOME_CARE_ALLOWANCE" to IncomeType("Kotihoidontuki", 1, false, false),
+            "RELATIVE_CARE_SUPPORT" to IncomeType("Omaishoidontuki", 1, false, false),
+            "STUDENT_INCOME" to IncomeType("Opiskelijan tulot", 1, false, false),
+            "GRANT" to IncomeType("Apuraha", 1, false, false),
+            "STARTUP_GRANT" to IncomeType("Starttiraha", 1, false, false),
+            "CAPITAL_INCOME" to IncomeType("Pääomatulo", 1, false, false),
+            "RENTAL_INCOME" to IncomeType("Vuokratulot", 1, false, false)
+        )
+    }
 }
