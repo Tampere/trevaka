abd48626a5a7a6bc1649740482856081e3a88ef5
anttipeltola <antti.peltola@gofore.com>
2021-09-17 13:05:49 +0300

    TREV-295 add missing license file, lint fixes

--- frontend/tampere/common.tsx
+++ frontend/tampere/common.tsx
@@ -1,8 +1,10 @@
-{/*
+{
+  /*
 SPDX-FileCopyrightText: 2021 City of Tampere
 
 SPDX-License-Identifier: LGPL-2.1-or-later
-*/}
+*/
+}
 
 import { CommonCustomizations } from 'lib-customizations/types'
 import theme from './tampere-theme'
