cfdf6e163eb95bb3ea8fc67e9c3731e998d25247
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-10-29 10:58:26 +0300

    Enable freeze voucher value reports api without dev profile

--- service/src/main/kotlin/fi/tampere/trevaka/dev/TampereDevApi.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/dev/TampereDevApi.kt
@@ -5,7 +5,6 @@
 package fi.tampere.trevaka.dev
 
 import fi.espoo.evaka.ExcludeCodeGen
-import fi.espoo.evaka.reports.freezeVoucherValueReportRows
 import fi.espoo.evaka.shared.async.AsyncJob
 import fi.espoo.evaka.shared.async.AsyncJobRunner
 import fi.espoo.evaka.shared.db.Database
@@ -13,12 +12,10 @@ import fi.tampere.trevaka.database.resetTampereDatabaseForE2ETests
 import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Duration
-import java.time.Instant
 
 @Profile("enable_dev_api")
 @RestController
@@ -42,10 +39,5 @@ class TampereDevApi(
             it.resetTampereDatabaseForE2ETests()
         }
         return ResponseEntity.noContent().build()
-    }
-
-    @PostMapping("/freeze-voucher-value-reports/{year}/{month}")
-    fun freezeVoucherValueReports(db: Database.Connection, @PathVariable year: Int, @PathVariable month: Int) {
-        db.transaction { tx -> freezeVoucherValueReportRows(tx, year, month, Instant.now()) }
     }
 }
