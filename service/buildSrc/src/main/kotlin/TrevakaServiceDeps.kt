14aecc16415fc759b4fcdaa84dc8fd1d891a0a29
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-11-23 08:25:16 +0200

    Fix dependencies

--- service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
+++ service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
@@ -3,12 +3,11 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 object TrevakaServiceDeps {
-    const val kotlin = "1.5.31"
-    const val springBoot = "2.5.6"
-    const val springCloud = "2020.0.2"
+    const val kotlin = "1.6.0"
+    const val springBoot = "2.6.0"
+    const val springCloud = "2020.0.4"
     const val java = "11"
     const val xjc = "2.0.0"
     const val junitPioneer = "1.3.8"
-    const val mockitoKotlin = "3.1.0"
     const val reflections = "0.9.12"
 }
