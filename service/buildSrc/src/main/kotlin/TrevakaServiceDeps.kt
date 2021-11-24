a08ddd1a35a893fd582cc00612e9db3832bed78c
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-11-24 09:56:05 +0200

    Java 17

--- service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
+++ service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
@@ -6,7 +6,7 @@ object TrevakaServiceDeps {
     const val kotlin = "1.6.0"
     const val springBoot = "2.6.0"
     const val springCloud = "2020.0.4"
-    const val java = "11"
+    const val java = "17"
     const val xjc = "2.0.0"
     const val junitPioneer = "1.3.8"
     const val reflections = "0.9.12"
