a3c78752200d89b07ca29296d44b4c5e3539d344
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-12-01 16:16:10 +0200

    Add child income

--- frontend/tampere/featureFlags.tsx
+++ frontend/tampere/featureFlags.tsx
@@ -9,6 +9,7 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 import { FeatureFlags } from 'lib-customizations/types'
 
 const featureFlags: FeatureFlags = {
+  childIncomeEnabled: true,
   daycareApplication: {
     dailyTimesEnabled: false
   },
