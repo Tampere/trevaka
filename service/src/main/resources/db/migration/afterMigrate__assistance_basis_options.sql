1fd4aae8ec0679456322b5a2be363014239b2318
Jarkko Pätäri <jarkko.patari@gofore.com>
2021-12-09 13:45:25 +0200

    Update assistance descriptions

--- service/src/main/resources/db/migration/afterMigrate__assistance_basis_options.sql
+++ service/src/main/resources/db/migration/afterMigrate__assistance_basis_options.sql
@@ -5,9 +5,9 @@
 INSERT INTO assistance_basis_option
     (value, name_fi, description_fi, display_order)
 VALUES
-    ('CHILD_ACCULTURATION_SUPPORT', 'Lapsen ja perheen kotoutumisen tuki', NULL, 100),
-    ('INTENSIFIED_ASSISTANCE', 'Tehostettu tuki', 'Ei päätöstä', 200),
-    ('SPECIAL_ASSISTANCE_DECISION', 'Erityinen tuki', 'Ei päätöstä', 300),
+    ('CHILD_ACCULTURATION_SUPPORT', 'Lapsen ja perheen kotoutumisen tuki', 'Lapsen ja perheen kotoutumisen tuki voidaan myöntää, kun perheen lapsi tulee ensimmäistä kertaa suomalaiseen päiväkotiin. Jos perheen muita lapsia on tällä hetkellä tai on ollut aiemmin suomalaisessa päiväkodissa, kotoutumisen tukea ei enää myönnetä. Pakolaistaustaisen perheen ollessa kyseessä aika on 6 kk, muiden osalta 3kk.', 100),
+    ('INTENSIFIED_ASSISTANCE', 'Tehostettu tuki', NULL, 200),
+    ('SPECIAL_ASSISTANCE_DECISION', 'Erityinen tuki', NULL, 300),
     ('DEVELOPMENTAL_DISABILITY', 'Kehitysvamma', NULL, 310),
     ('EXTENDED_COMPULSORY_EDUCATION', 'Pidennetty oppivelvollisuus', 'Lapsella erityisen tuen päätös', 320)
 ON CONFLICT (value) DO
@@ -17,5 +17,5 @@ UPDATE SET
     display_order = EXCLUDED.display_order
 WHERE
     assistance_basis_option.name_fi <> EXCLUDED.name_fi OR
-    assistance_basis_option.description_fi <> EXCLUDED.description_fi OR
+    assistance_basis_option.description_fi IS DISTINCT FROM EXCLUDED.description_fi OR
     assistance_basis_option.display_order <> EXCLUDED.display_order;
