5ccace67355182eb0d14fff5ec4294e71b347ef4
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-02-02 12:09:16 +0200

    Add invoice generator config

--- service/src/main/kotlin/fi/tampere/trevaka/TampereConfig.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/TampereConfig.kt
@@ -1,5 +1,8 @@
 package fi.tampere.trevaka
 
+import fi.espoo.evaka.invoicing.service.InvoiceGenerator
+import fi.espoo.evaka.invoicing.service.InvoiceProductProvider
+import fi.espoo.evaka.invoicing.service.NewDraftInvoiceGenerator
 import fi.espoo.evaka.shared.FeatureConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,5 +17,9 @@ class TampereConfig {
         citizenReservationThresholdHours = 6 * 24, // Tue 00:00
         dailyFeeDivisorOperationalDaysOverride = 20
     )
+
+    @Bean
+    fun invoiceGenerator(productProvider: InvoiceProductProvider, featureConfig: FeatureConfig): InvoiceGenerator =
+        InvoiceGenerator(NewDraftInvoiceGenerator(productProvider, featureConfig))
 
 }
