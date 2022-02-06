25f7d9b2fbb6aecf243392f21233ea5da7efdaa0
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-02-06 11:20:55 +0200

    Sync dependencies with upstream

--- service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
+++ service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 object TrevakaServiceDeps {
-    const val kotlin = "1.6.0"
-    const val springBoot = "2.6.0"
+    const val kotlin = "1.6.10"
+    const val springBoot = "2.6.2"
     const val springCloud = "2020.0.4"
     const val java = "17"
     const val xjc = "2.0.0"
