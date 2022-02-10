4c1701307ef6d72b91e855d86b0db83d10ba076c
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-02-10 12:57:24 +0200

    Add assistance need/action read with staff role

--- service/src/main/kotlin/fi/tampere/trevaka/security/TamperePermittedRoleActions.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/security/TamperePermittedRoleActions.kt
@@ -1,6 +1,20 @@
 package fi.tampere.trevaka.security
 
+import fi.espoo.evaka.shared.auth.UserRole
+import fi.espoo.evaka.shared.security.Action
 import fi.espoo.evaka.shared.security.PermittedRoleActions
+import java.util.EnumSet
 
 class TamperePermittedRoleActions(private val defaults: PermittedRoleActions) : PermittedRoleActions by defaults {
+    override fun childActions(role: UserRole): Set<Action.Child> {
+        val added = addedChildActionsByRole[role] ?: emptySet()
+        return defaults.childActions(role) + added
+    }
 }
+
+private val addedChildActionsByRole: Map<UserRole, Set<Action.Child>> = mapOf(
+    UserRole.STAFF to EnumSet.of(
+        Action.Child.READ_ASSISTANCE_NEED,
+        Action.Child.READ_ASSISTANCE_ACTION
+    )
+)
