3d9d7d82256b2897943ecb456827f6d8b6459f01
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-02-10 13:03:10 +0200

    Add backup care crud with staff role

--- service/src/main/kotlin/fi/tampere/trevaka/security/TamperePermittedRoleActions.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/security/TamperePermittedRoleActions.kt
@@ -6,14 +6,27 @@ import fi.espoo.evaka.shared.security.PermittedRoleActions
 import java.util.EnumSet
 
 class TamperePermittedRoleActions(private val defaults: PermittedRoleActions) : PermittedRoleActions by defaults {
+    override fun backupCareActions(role: UserRole): Set<Action.BackupCare> {
+        val added = addedBackupCareActionsByRole[role] ?: emptySet()
+        return defaults.backupCareActions(role) + added
+    }
+
     override fun childActions(role: UserRole): Set<Action.Child> {
         val added = addedChildActionsByRole[role] ?: emptySet()
         return defaults.childActions(role) + added
     }
 }
 
+private val addedBackupCareActionsByRole: Map<UserRole, Set<Action.BackupCare>> = mapOf(
+    UserRole.STAFF to EnumSet.of(
+        Action.BackupCare.UPDATE,
+        Action.BackupCare.DELETE
+    ),
+)
+
 private val addedChildActionsByRole: Map<UserRole, Set<Action.Child>> = mapOf(
     UserRole.STAFF to EnumSet.of(
+        Action.Child.CREATE_BACKUP_CARE,
         Action.Child.READ_ASSISTANCE_NEED,
         Action.Child.READ_ASSISTANCE_ACTION
     )
