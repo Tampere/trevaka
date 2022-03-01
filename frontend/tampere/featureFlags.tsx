f7e77ce8533a9fc2d623487791c2655e45d4f6a4
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-03-01 08:45:13 +0200

    Fix feature flags

--- frontend/tampere/featureFlags.tsx
+++ frontend/tampere/featureFlags.tsx
@@ -9,7 +9,6 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 import { FeatureFlags } from 'lib-customizations/types'
 
 const featureFlags: FeatureFlags = {
-  childIncomeEnabled: true,
   daycareApplication: {
     dailyTimesEnabled: false
   },
