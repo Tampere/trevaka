17dc43ad21c6c869329bca5496f1faaed710660f
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-03-04 07:46:09 +0200

    Add datadog

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -6,8 +6,9 @@
 
 set -euo pipefail
 
-# For logback tagging (allow for local fallback)
-export HOST_IP=$(wget -qO- http://169.254.169.254/latest/meta-data/local-ipv4 || printf 'UNAVAILABLE')
+# For log tagging (with a default value and error logging without crashing)
+# shellcheck disable=SC2155
+export HOST_IP=$(curl --silent --fail --show-error http://169.254.169.254/latest/meta-data/local-ipv4 || printf 'UNAVAILABLE')
 
 # Download deployment specific files from S3 if in a non-local environment
 if [ "${VOLTTI_ENV:-X}" != "local" ]; then
@@ -16,4 +17,28 @@ fi
 
 # Run as exec so the application can receive any Unix signals sent to the container, e.g.,
 # Ctrl + C.
-exec java -cp . -server $JAVA_OPTS org.springframework.boot.loader.JarLauncher "$@"
+if [ "${DD_PROFILING_ENABLED:-false}" = "true" ]; then
+  export DD_AGENT_HOST="${DD_AGENT_HOST:-localhost}"
+  export DD_TRACE_AGENT_PORT="${DD_TRACE_AGENT_PORT:-8126}"
+  export DD_ENV="${DD_ENV:-$VOLTTI_ENV}"
+  export DD_VERSION="${DD_VERSION:-$APP_COMMIT}"
+  export DD_SERVICE="${DD_SERVICE:-$APP_NAME}"
+
+  if [ "$DD_AGENT_HOST" = "UNAVAILABLE" ]; then
+    echo "Invalid DD_AGENT_HOST. Is it unset and not in AWS environment?"
+    exit 1
+  fi
+
+  # shellcheck disable=SC2086
+  exec java \
+    -Ddd.jmxfetch.config=/etc/jmxfetch/conf.yaml \
+    -Ddd.profiling.enabled=true \
+    -Ddd.logs.injection=true \
+    -Ddd.trace.sample.rate=1 \
+    -javaagent:/opt/dd-java-agent.jar \
+    -XX:FlightRecorderOptions=stackdepth=256 \
+    -cp . -server $JAVA_OPTS org.springframework.boot.loader.JarLauncher "$@"
+else
+  # shellcheck disable=SC2086
+  exec java -cp . -server $JAVA_OPTS org.springframework.boot.loader.JarLauncher "$@"
+fi
