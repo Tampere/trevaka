86f6e969148a472eea3706f0fb99bf7cda59953c
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-04-04 16:36:51 +0300

    Update evaka submodule

--- service/src/test/kotlin/fi/tampere/trevaka/invoice/service/TrevakaInvoiceClientTest.kt
+++ service/src/test/kotlin/fi/tampere/trevaka/invoice/service/TrevakaInvoiceClientTest.kt
@@ -178,7 +178,8 @@ fun validInvoice(): InvoiceDetailed {
         ), 1, 24300,
         LocalDate.of(2021, 1, 1),
         LocalDate.of(2021, 1, 31),
-        ProductKey("DAYCARE"), DaycareId(UUID.randomUUID()), "131885", null, null, "kuvaus1"
+        ProductKey("DAYCARE"), DaycareId(UUID.randomUUID()), "131885", null, null, "kuvaus1",
+        correctionId = null
     )
     val invoiceRow2 = InvoiceRowDetailed(
         InvoiceRowId(UUID.randomUUID()), PersonDetailed(
@@ -189,7 +190,8 @@ fun validInvoice(): InvoiceDetailed {
         ), 1, 48200,
         LocalDate.of(2021, 1, 1),
         LocalDate.of(2021, 1, 31),
-        ProductKey("PRESCHOOL_WITH_DAYCARE"), DaycareId(UUID.randomUUID()), "284823", null, null, "kuvaus2"
+        ProductKey("PRESCHOOL_WITH_DAYCARE"), DaycareId(UUID.randomUUID()), "284823", null, null, "kuvaus2",
+        correctionId = null
     )
     return InvoiceDetailed(
         (InvoiceId(UUID.randomUUID())),
