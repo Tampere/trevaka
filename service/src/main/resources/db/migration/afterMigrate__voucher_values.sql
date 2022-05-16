e27779d767286e901b92d65e5a9fa6619d04c5af
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-05-16 15:34:22 +0300

    Add 2022 voucher values

--- service/src/main/resources/db/migration/afterMigrate__voucher_values.sql
+++ service/src/main/resources/db/migration/afterMigrate__voucher_values.sql
@@ -5,7 +5,8 @@
 INSERT INTO voucher_value
     (id, validity, base_value, base_value_age_under_three)
 VALUES
-    ('084314dc-ed7f-4725-92f2-5c220bb4bb7e', daterange('2000-01-01', NULL, '[]'), 82600, 124700)
+    ('084314dc-ed7f-4725-92f2-5c220bb4bb7e', daterange('2000-01-01', '2022-07-31', '[]'), 82600, 124700),
+    ('2f39fec0-d514-11ec-9d64-0242ac120002', daterange('2022-08-01', NULL, '[]'), 86200, 130200)
 ON CONFLICT (id) DO
 UPDATE SET
     validity = EXCLUDED.validity,
