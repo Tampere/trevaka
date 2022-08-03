dfb73c4add74acbf1d01dbf097c8ca6c69bafd00
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-08-03 09:12:17 +0300

    Upgrade to cas 6.5.7

--- mock/identification/build.gradle.kts
+++ mock/identification/build.gradle.kts
@@ -5,8 +5,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.6.3"
     id("io.freefair.lombok") version "6.3.0"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
@@ -22,41 +21,12 @@ repositories {
     maven(url = "https://build.shibboleth.net/nexus/content/repositories/releases/")
 }
 
-dependencyManagement {
-    imports {
-        mavenBom("org.apereo.cas:cas-server-support-bom:6.4.4")
-    }
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // minimal cas installation from https://github.com/apereo/cas/blob/v6.4.4/gradle/webapp.gradle
-    implementation("org.apereo.cas:cas-server-core")
-    implementation("org.apereo.cas:cas-server-core-audit")
-    implementation("org.apereo.cas:cas-server-core-authentication")
-    implementation("org.apereo.cas:cas-server-core-configuration")
-    implementation("org.apereo.cas:cas-server-core-cookie")
-    implementation("org.apereo.cas:cas-server-core-logout")
-    implementation("org.apereo.cas:cas-server-core-logging")
-    implementation("org.apereo.cas:cas-server-core-services")
-    implementation("org.apereo.cas:cas-server-core-tickets")
-    implementation("org.apereo.cas:cas-server-core-util")
-    implementation("org.apereo.cas:cas-server-core-validation")
-    implementation("org.apereo.cas:cas-server-core-web")
-    implementation("org.apereo.cas:cas-server-core-notifications")
-    implementation("org.apereo.cas:cas-server-support-jpa-util")
-    implementation("org.apereo.cas:cas-server-support-actions")
-    implementation("org.apereo.cas:cas-server-support-person-directory")
-    implementation("org.apereo.cas:cas-server-support-themes")
-    implementation("org.apereo.cas:cas-server-support-validation")
-    implementation("org.apereo.cas:cas-server-support-thymeleaf")
-    implementation("org.apereo.cas:cas-server-support-pm-webflow")
-    implementation("org.apereo.cas:cas-server-webapp-config")
-    implementation("org.apereo.cas:cas-server-webapp-init")
-    implementation("org.apereo.cas:cas-server-webapp-resources")
-    implementation("org.apereo.cas:cas-server-webapp-init-tomcat")
+    implementation(platform("org.apereo.cas:cas-server-support-bom:6.5.7"))
+    implementation("org.apereo.cas:cas-server-webapp-starter-tomcat")
 
     implementation("org.apereo.cas:cas-server-support-generic")
     implementation("org.apereo.cas:cas-server-support-yaml-service-registry")
