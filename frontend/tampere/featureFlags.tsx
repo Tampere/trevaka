224b6b3518e89f00c08c8dd005f1bf27a6cf52b2
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-08-08 11:15:32 +0300

    Disable assistance need decisions in prod

--- frontend/tampere/featureFlags.tsx
+++ frontend/tampere/featureFlags.tsx
@@ -8,25 +8,77 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 
 import { FeatureFlags } from 'lib-customizations/types'
 
-const featureFlags: FeatureFlags = {
-  citizenShiftCareAbsenceEnabled: false,
-  daycareApplication: {
-    dailyTimesEnabled: false
-  },
-  groupsTableServiceNeedsEnabled: true,
-  evakaLogin: true,
-  financeBasicsPage: true,
-  urgencyAttachmentsEnabled: false,
-  preschoolEnabled: false,
-  assistanceActionOtherEnabled: false,
-  experimental: {
-    messageAttachments: true,
-    personalDetailsPage: true,
-    mobileMessages: true,
-    assistanceNeedDecisions: true,
-    staffAttendanceTypes: true
-  },
-  adminSettingsEnabled: true
+import { env, Env } from './env'
+
+type Features = {
+  default: FeatureFlags
+} & {
+  [k in Env]: FeatureFlags
 }
+
+const features: Features = {
+  default: {
+    citizenShiftCareAbsenceEnabled: false,
+    daycareApplication: {
+      dailyTimesEnabled: false
+    },
+    groupsTableServiceNeedsEnabled: true,
+    evakaLogin: true,
+    financeBasicsPage: true,
+    urgencyAttachmentsEnabled: false,
+    preschoolEnabled: false,
+    assistanceActionOtherEnabled: false,
+    experimental: {
+      messageAttachments: true,
+      personalDetailsPage: true,
+      mobileMessages: true,
+      assistanceNeedDecisions: true,
+      staffAttendanceTypes: true
+    },
+    adminSettingsEnabled: true
+  },
+  staging: {
+    citizenShiftCareAbsenceEnabled: false,
+    daycareApplication: {
+      dailyTimesEnabled: false
+    },
+    groupsTableServiceNeedsEnabled: true,
+    evakaLogin: true,
+    financeBasicsPage: true,
+    urgencyAttachmentsEnabled: false,
+    preschoolEnabled: false,
+    assistanceActionOtherEnabled: false,
+    experimental: {
+      messageAttachments: true,
+      personalDetailsPage: true,
+      mobileMessages: true,
+      assistanceNeedDecisions: false,
+      staffAttendanceTypes: true
+    },
+    adminSettingsEnabled: true
+  },
+  prod: {
+    citizenShiftCareAbsenceEnabled: false,
+    daycareApplication: {
+      dailyTimesEnabled: false
+    },
+    groupsTableServiceNeedsEnabled: true,
+    evakaLogin: true,
+    financeBasicsPage: true,
+    urgencyAttachmentsEnabled: false,
+    preschoolEnabled: false,
+    assistanceActionOtherEnabled: false,
+    experimental: {
+      messageAttachments: true,
+      personalDetailsPage: true,
+      mobileMessages: true,
+      assistanceNeedDecisions: false,
+      staffAttendanceTypes: true
+    },
+    adminSettingsEnabled: true
+  }
+}
+
+const featureFlags = features[env()]
 
 export default featureFlags
