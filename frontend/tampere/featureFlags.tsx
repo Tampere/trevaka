f41a5a36e44ed60cb989e5f8569c0df638c26bb5
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-08-23 10:21:58 +0300

    Enable citizen vasu (local & dev)

--- frontend/tampere/featureFlags.tsx
+++ frontend/tampere/featureFlags.tsx
@@ -32,6 +32,7 @@ const features: Features = {
       messageAttachments: true,
       personalDetailsPage: true,
       mobileMessages: true,
+      citizenVasu: true,
       assistanceNeedDecisions: true,
       staffAttendanceTypes: true
     },
