f73320cec05c344c3c785422da2314ab1c2fe6af
Jarkko Pätäri <jarkko.patari@gofore.com>
2022-12-14 13:09:57 +0200

    Upgrade identification mock to cas 6.6.3

--- mock/identification/build.gradle.kts
+++ mock/identification/build.gradle.kts
@@ -5,10 +5,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.3"
-    id("io.freefair.lombok") version "6.3.0"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
+    id("org.springframework.boot") version "2.7.3"
+    id("io.freefair.lombok") version "6.6"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
 }
 
 group = "com.example"
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation(platform("org.apereo.cas:cas-server-support-bom:6.5.7"))
+    implementation(platform("org.apereo.cas:cas-server-support-bom:6.6.3"))
     implementation("org.apereo.cas:cas-server-webapp-starter-tomcat")
 
     implementation("org.apereo.cas:cas-server-support-generic")
