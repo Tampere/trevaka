94b071719ac62345b011f5eae934571ea41d8dd4
Jarkko Pätäri <jarkko.patari@gofore.com>
2023-02-06 07:56:04 +0200

    Upgrade to kotlin 1.8.0

--- service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
+++ service/buildSrc/src/main/kotlin/TrevakaServiceDeps.kt
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 object TrevakaServiceDeps {
-    const val kotlin = "1.7.21"
+    const val kotlin = "1.8.0"
     const val springBoot = "3.0.2"
     const val springCloud = "2022.0.1"
     const val java = "17"
