54045a137ab710a603d81d034e6bf72390870190
Jarkko Pätäri <jarkko.patari@gofore.com>
2023-05-12 13:08:53 +0300

    Revert "Merge pull request #174 from Tampere/info-endpoint"
    This reverts commit 690360d4c1255e21873271c0bcb3265412289278, reversing
    changes made to f265fc59979f7ce277e0ee3262f25ef7fe074da9.

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.boot)
     id("org.unbroken-dome.xjc") version "2.0.0"
-    id("com.gorylenko.gradle-git-properties") version "2.4.1"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
