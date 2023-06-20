bd111be0d6a03a0e6bbee7e45e073a6b65a3a4a3
Jarkko Pätäri <jarkko.patari@gofore.com>
2023-06-20 14:28:29 +0300

    Add new assistance model

--- service/src/main/resources/db/migration/afterMigrate__assistance_action_options.sql
+++ service/src/main/resources/db/migration/afterMigrate__assistance_action_options.sql
@@ -5,16 +5,18 @@
 INSERT INTO assistance_action_option
     (value, name_fi, description_fi, display_order)
 VALUES
-    ('10', 'Avustajapalvelut', 'Lapsen ryhmässä työskentelee ryhmäavustaja tai lapsella on henkilökohtainen avustaja. Mikäli lapsi on integroidussa varhaiskasvatusryhmässä tai erityisryhmässä, tätä vaihtoehtoa ei valita.', 10),
-    ('20', 'Erho', NULL, 20),
-    ('30', 'Erityisryhmä', 'Lapsi on erityisryhmässä.', 30),
-    ('40', 'Henkilökuntalisäys', 'Lapsen ryhmässä työskentelee lain vaatimaa resurssia enemmän varhaiskasvatuksen lastenhoitajia/opettajia. Mikäli lapsi on integroidussa varhaiskasvatusryhmässä tai erityisryhmässä, tätä vaihtoehtoa ei valita.', 40),
-    ('50', 'Integroitu varhaiskasvatusryhmä ', 'Lapsi on integroidussa varhaiskasvatusryhmässä.', 50),
-    ('60', 'Osa-aikainen erityisopetus', 'Lapsi saa osa-aikaista erityisopetusta. Mikäli lapsi on integroidussa varhaiskasvatusryhmässä tai erityisryhmässä, tätä vaihtoehtoa ei valita.', 60),
-    ('70', 'Veon tuki', 'Lapsen ryhmän henkilökunta saa veon tukea ja konsultaatiota ja/tai veo on samanaikaisopettajana lapsen ryhmässä. Mikäli lapsi on integroidussa varhaiskasvatusryhmässä tai erityisryhmässä, tätä vaihtoehtoa ei valita.', 70),
-    ('80', 'Tulkitsemispalvelut', 'Lapsi saa tulkitsemispalveluita. Huoltajien kanssa käytettävät tulkkipalvelut eivät sisälly tähän.', 80),
-    ('90', 'Kuljetusetu', 'Lapsi on saanut päätöksen kuljetusedusta.', 90),
-    ('100', 'Ryhmän pienennys', 'Ryhmän pienennys rakenteellisen tuen muotona eli lapsiryhmää pienennetään, jotta lasten tuki toteutuu tarkoituksenmukaisesti. Kirjaa myös tuen kerroin sille varattuun paikkaan.', 100)
+    ('10', 'Avustajapalvelut', 'Lapsen ryhmässä työskentelee ryhmäavustaja tai lapsella on henkilökohtainen avustaja.', 80),
+    ('20', 'Erho', NULL, 110),
+    ('30', 'Varhaiskasvatuksen erityisryhmä', 'Lapsi on erityisryhmässä.', 10),
+    ('35', 'Esiopetuksen erityisryhmä', NULL, 20),
+    ('40', 'Henkilökuntalisäys tai -muutos', 'Henkilökuntalisäys: Lapsen ryhmässä työskentelee lain vaatimaan resurssia enemmän varhaiskasvatuksen lastenhoitajia/sosionomeja/opettajia. Henkilökuntamuutos: Ryhmää on vahvistettu pedagogisesti siten, että henkilöstöön kuuluu kaksi varhaiskasvatuksen opettajaa. Mikäli lapsi on erityisryhmässä tai integroidussa ryhmässä, tätä vaihtoehtoa ei valita. Älä valitse tätä vaihtoehtoa esiopetuksessa.', 90),
+    ('50', 'Integroitu varhaiskasvatusryhmä', 'Lapsi on integroidussa varhaiskasvatusryhmässä.', 30),
+    ('55', 'Integroitu esiopetusryhmä', 'Lapsi on integroidussa esiopetusryhmässä.', 40),
+    ('57', 'Kokoaikainen erityisopetus', 'Lapsen ryhmässä työskentelee yhtenä työntekijänä varhaiskasvatuksen erityisopettaja.', 50),
+    ('60', 'Osa-aikainen erityisopetus', 'Lapsi saa osa-aikaista erityisopetusta.', 60),
+    ('70', 'Erityisopettajan konsultaatio', 'Lapsen ryhmän henkilökunta saa erityisopettajan konsultaatiota.', 70),
+    ('80', 'Tulkitsemispalvelut', 'Lapsi saa tulkitsemispalveluita kuulo- ja/tai näkövammansa vuoksi. Huoltajien kanssa käytettävät tulkkipalvelut eivät sisälly tähän.', 120),
+    ('100', 'Ryhmän pienennys', 'Ryhmän pienennys rakenteellisen tuen muotona eli lapsiryhmää pienennetään, jotta lasten tuki toteutuu tarkoituksenmukaisesti. Kirjaa myös tuen kerroin kohtaan Tuen tarve. Älä valitse tätä vaihtoehtoa esiopetuksessa.', 100)
 ON CONFLICT (value) DO
 UPDATE SET
     name_fi = EXCLUDED.name_fi,
---
+++ service/src/test/resources/db/migration/afterMigrate__assistance_data_migration.sql
@@ -0,0 +1,50 @@
+-- SPDX-FileCopyrightText: 2021-2023 City of Tampere
+--
+-- SPDX-License-Identifier: LGPL-2.1-or-later
+
+CREATE OR REPLACE TEMPORARY VIEW assistance_need_view AS
+SELECT child_id, need.updated AS modified, updated_by AS modified_by, daterange(start_date, end_date, '[]') AS valid_during, option.value
+FROM assistance_basis_option_ref ref
+JOIN assistance_basis_option option ON option.id = ref.option_id
+JOIN assistance_need need ON need.id = ref.need_id;
+
+-- assistance factor
+INSERT INTO assistance_factor (child_id, modified, modified_by, valid_during, capacity_factor)
+SELECT child_id, updated AS modified, updated_by AS modified_by, daterange(start_date, end_date, '[]') AS valid_during, capacity_factor
+FROM assistance_need;
+
+-- daycare assistance
+INSERT INTO daycare_assistance (child_id, modified, modified_by, valid_during, level)
+SELECT child_id, modified, modified_by, valid_during, 'INTENSIFIED_SUPPORT'
+FROM assistance_need_view WHERE value = 'INTENSIFIED_ASSISTANCE';
+
+INSERT INTO daycare_assistance (child_id, modified, modified_by, valid_during, level)
+SELECT child_id, modified, modified_by, valid_during, 'SPECIAL_SUPPORT'
+FROM assistance_need_view WHERE value = 'SPECIAL_ASSISTANCE_DECISION';
+
+-- preschool assistance
+INSERT INTO preschool_assistance (child_id, modified, modified_by, valid_during, level)
+SELECT child_id, modified, modified_by, valid_during, 'SPECIAL_SUPPORT_WITH_DECISION_LEVEL_1'
+FROM assistance_need_view WHERE value = 'EXTENDED_COMPULSORY_EDUCATION';
+
+-- other assistance measure
+WITH
+deleted_transport_benefits AS (
+    DELETE FROM assistance_action_option_ref ref
+    USING assistance_action_option option, assistance_action action
+    WHERE option.id = ref.option_id AND action.id = ref.action_id AND option.value = '90'
+    RETURNING child_id, action.updated AS modified, updated_by AS modified_by, daterange(start_date, end_date, '[]') AS valid_during
+)
+INSERT INTO other_assistance_measure (child_id, modified, modified_by, valid_during, type)
+SELECT child_id, modified, modified_by, valid_during, 'TRANSPORT_BENEFIT'
+FROM deleted_transport_benefits;
+
+INSERT INTO other_assistance_measure (child_id, modified, modified_by, valid_during, type)
+SELECT child_id, modified, modified_by, valid_during, 'ACCULTURATION_SUPPORT'
+FROM assistance_need_view WHERE value = 'CHILD_ACCULTURATION_SUPPORT';
+
+-- cleanup
+DELETE FROM assistance_action action
+WHERE NOT EXISTS (SELECT FROM assistance_action_option_ref ref WHERE ref.action_id = action.id);
+
+DELETE FROM assistance_action_option WHERE value = '90';
