79cbed6b21a996a23d8c9fdce12a4e96e7efaeb5
Tatu Repo <tatu.repo@gofore.com>
2023-06-22 12:01:04 +0300

    Formatting

--- service/src/main/kotlin/fi/tampere/trevaka/security/TampereActionRuleMapping.kt
+++ service/src/main/kotlin/fi/tampere/trevaka/security/TampereActionRuleMapping.kt
@@ -401,7 +401,8 @@ class TampereActionRuleMapping : ActionRuleMapping {
         Action.Unit.READ_TEMPORARY_EMPLOYEE,
         Action.Unit.CREATE_TEMPORARY_EMPLOYEE,
         Action.Unit.UPDATE_TEMPORARY_EMPLOYEE,
-        Action.Unit.DELETE_TEMPORARY_EMPLOYEE -> {
+        Action.Unit.DELETE_TEMPORARY_EMPLOYEE,
+        -> {
             @Suppress("UNCHECKED_CAST")
             action.defaultRules.asSequence() + sequenceOf(
                 HasUnitRole(UserRole.EARLY_CHILDHOOD_EDUCATION_SECRETARY).inUnit() as ScopedActionRule<in T>,
