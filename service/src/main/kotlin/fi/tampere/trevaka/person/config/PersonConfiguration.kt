Tampere/trevaka

commit 49cf103965a6acc212b920874ae833a60b18b0ee

Refactor service components

service/src/main/kotlin/fi/tampere/trevaka/person/config/PersonConfiguration.kt
@@ -5,19 +5,15 @@
 package fi.tampere.trevaka.person.config
 
 import com.github.kittinunf.fuel.core.FuelManager
-import com.github.kittinunf.fuel.core.extensions.authentication
-import fi.espoo.evaka.dvv.DvvModificationRequestCustomizer
 import fi.tampere.trevaka.TampereProperties
-import fi.tampere.trevaka.util.NoConnectionReuseStrategy
-import fi.tampere.trevaka.util.basicAuthInterceptor
 import org.apache.hc.client5.http.classic.HttpClient
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.ws.transport.WebServiceMessageSender
 import org.springframework.ws.transport.http.HttpComponents5MessageSender
-import org.springframework.ws.transport.http.HttpComponents5MessageSender.RemoveSoapHeadersInterceptor
+import trevaka.ipaas.dvvModificationRequestCustomizer
+import trevaka.ipaas.newIpaasHttpClient
 
 const val HTTP_CLIENT_PERSON = "httpClientPerson"
 
@@ -33,20 +29,13 @@ class PersonConfiguration {
     }
 
     @Bean(HTTP_CLIENT_PERSON)
-    fun httpClient(properties: TampereProperties) = HttpClientBuilder.create()
-        .addRequestInterceptorFirst(RemoveSoapHeadersInterceptor())
-        .addRequestInterceptorFirst(basicAuthInterceptor(properties.ipaas.username, properties.ipaas.password))
-        .setConnectionReuseStrategy(NoConnectionReuseStrategy.INSTANCE) // fix random "connection reset" errors
-        .build()
+    fun httpClient(properties: TampereProperties) = newIpaasHttpClient(properties.ipaas)
 
     /**
      * Custom [FuelManager] for [fi.espoo.evaka.dvv.DvvModificationsServiceClient].
      */
-    @Bean
-    fun fuelManager(properties: TampereProperties) = FuelManager()
+    fun fuelManager() = FuelManager()
 
     @Bean
-    fun basicAuthCustomizer(properties: TampereProperties) = DvvModificationRequestCustomizer { request ->
-        request.authentication().basic(properties.ipaas.username, properties.ipaas.password)
-    }
+    fun basicAuthCustomizer(properties: TampereProperties) = dvvModificationRequestCustomizer(properties.ipaas)
 }

service/src/main/kotlin/fi/tampere/trevaka/TampereConfig.kt
@@ -5,17 +5,16 @@
 package fi.tampere.trevaka
 
 import fi.espoo.evaka.invoicing.domain.PaymentIntegrationClient
-import fi.espoo.evaka.logging.defaultAccessLoggingValve
 import fi.espoo.evaka.shared.FeatureConfig
 import fi.espoo.evaka.shared.auth.UserRole
 import fi.espoo.evaka.shared.security.actionrule.ActionRuleMapping
 import fi.espoo.evaka.titania.TitaniaEmployeeIdConverter
 import fi.tampere.trevaka.security.TampereActionRuleMapping
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
+import trevaka.titania.TrimStartTitaniaEmployeeIdConverter
+import trevaka.tomcat.tomcatAccessLoggingCustomizer
 
 @Configuration
 class TampereConfig {
@@ -54,13 +53,8 @@ class TampereConfig {
     fun actionRuleMapping(): ActionRuleMapping = TampereActionRuleMapping()
 
     @Bean
-    fun titaniaEmployeeIdConverter(): TitaniaEmployeeIdConverter = object : TitaniaEmployeeIdConverter {
-        override fun fromTitania(employeeId: String): String = employeeId.trimStart('0')
-    }
+    fun titaniaEmployeeIdConverter(): TitaniaEmployeeIdConverter = TrimStartTitaniaEmployeeIdConverter()
 
     @Bean
-    fun tomcatCustomizer(env: Environment) =
-        WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-            it.addContextValves(defaultAccessLoggingValve(env))
-        }
+    fun accessLoggingCustomizer(env: Environment) = tomcatAccessLoggingCustomizer(env)
 }

service/src/main/kotlin/fi/tampere/trevaka/TampereProperties.kt
@@ -5,6 +5,7 @@
 package fi.tampere.trevaka
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import trevaka.ipaas.IpaasProperties
 import java.time.Month
 
 /**
@@ -15,11 +16,6 @@ data class TampereProperties(
     val ipaas: IpaasProperties,
     val invoice: InvoiceProperties,
     val summertimeAbsence: SummertimeAbsenceProperties = SummertimeAbsenceProperties(),
-)
-
-data class IpaasProperties(
-    val username: String,
-    val password: String,
 )
 
 data class InvoiceProperties(

service/src/main/kotlin/fi/tampere/trevaka/invoice/config/InvoiceConfiguration.kt
@@ -21,10 +21,7 @@ import fi.espoo.evaka.shared.db.Database
 import fi.tampere.trevaka.SummertimeAbsenceProperties
 import fi.tampere.trevaka.TampereProperties
 import fi.tampere.trevaka.invoice.service.TampereInvoiceClient
-import fi.tampere.trevaka.util.NoConnectionReuseStrategy
-import fi.tampere.trevaka.util.basicAuthInterceptor
 import org.apache.hc.client5.http.classic.HttpClient
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -34,7 +31,7 @@ import org.springframework.ws.client.core.WebServiceTemplate
 import org.springframework.ws.soap.SoapVersion
 import org.springframework.ws.soap.saaj.SaajSoapMessageFactory
 import org.springframework.ws.transport.http.HttpComponents5MessageSender
-import org.springframework.ws.transport.http.HttpComponents5MessageSender.RemoveSoapHeadersInterceptor
+import trevaka.ipaas.newIpaasHttpClient
 import java.math.BigDecimal
 import java.time.Month
 
@@ -77,11 +74,7 @@ class InvoiceConfiguration {
     }
 
     @Bean(HTTP_CLIENT_INVOICE)
-    fun httpClient(properties: TampereProperties) = HttpClientBuilder.create()
-        .addRequestInterceptorFirst(RemoveSoapHeadersInterceptor())
-        .addRequestInterceptorFirst(basicAuthInterceptor(properties.ipaas.username, properties.ipaas.password))
-        .setConnectionReuseStrategy(NoConnectionReuseStrategy.INSTANCE) // fix random "connection reset" errors
-        .build()
+    fun httpClient(properties: TampereProperties) = newIpaasHttpClient(properties.ipaas)
 
     @Bean
     fun incomeTypesProvider(): IncomeTypesProvider = TampereIncomeTypesProvider()

service/src/main/kotlin/fi/tampere/trevaka/util/HttpUtil.kt
@@ -1,30 +1,0 @@
-// SPDX-FileCopyrightText: 2021 City of Tampere
-//
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-package fi.tampere.trevaka.util
-
-import org.apache.hc.core5.http.ConnectionReuseStrategy
-import org.apache.hc.core5.http.HttpRequest
-import org.apache.hc.core5.http.HttpRequestInterceptor
-import org.apache.hc.core5.http.HttpResponse
-import org.apache.hc.core5.http.message.BasicHeader
-import org.apache.hc.core5.http.protocol.HttpContext
-import java.nio.charset.StandardCharsets
-import java.util.Base64
-
-fun basicAuthInterceptor(username: String, password: String) = HttpRequestInterceptor { request, _, _ ->
-    request.addHeader(BasicHeader("Authorization", "Basic ${encode("$username:$password")}"))
-}
-
-internal fun encode(data: String) = Base64.getEncoder().encodeToString(data.toByteArray(StandardCharsets.UTF_8))
-
-class NoConnectionReuseStrategy : ConnectionReuseStrategy {
-    override fun keepAlive(request: HttpRequest?, response: HttpResponse?, context: HttpContext?): Boolean {
-        return false
-    }
-
-    companion object {
-        val INSTANCE = NoConnectionReuseStrategy()
-    }
-}

service/src/main/kotlin/trevaka/Main.kt
@@ -10,7 +10,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @SpringBootApplication(
     scanBasePackages = ["trevaka", "fi.espoo.evaka"],
@@ -27,3 +31,9 @@ class Main
 fun main(args: Array<String>) {
     runApplication<Main>(*args)
 }
+
+@Profile("tampere_evaka")
+@Configuration
+@ComponentScan("fi.tampere.trevaka")
+@ConfigurationPropertiesScan(basePackages = ["fi.tampere.trevaka"])
+class TampereComponentScan

service/src/main/kotlin/trevaka/TampereComponentScan.kt
@@ -1,16 +1,0 @@
-// SPDX-FileCopyrightText: 2021-2023 City of Tampere
-//
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-package trevaka
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-
-@Profile("tampere_evaka")
-@Configuration
-@ComponentScan("fi.tampere.trevaka")
-@ConfigurationPropertiesScan(basePackages = ["fi.tampere.trevaka"])
-class TampereComponentScan

service/src/main/kotlin/trevaka/ipaas/Ipaas.kt
@@ -1,0 +1,49 @@
+// SPDX-FileCopyrightText: 2021-2023 City of Tampere
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package trevaka.ipaas
+
+import com.github.kittinunf.fuel.core.extensions.authentication
+import fi.espoo.evaka.dvv.DvvModificationRequestCustomizer
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
+import org.apache.hc.core5.http.ConnectionReuseStrategy
+import org.apache.hc.core5.http.HttpRequest
+import org.apache.hc.core5.http.HttpRequestInterceptor
+import org.apache.hc.core5.http.HttpResponse
+import org.apache.hc.core5.http.message.BasicHeader
+import org.apache.hc.core5.http.protocol.HttpContext
+import org.springframework.ws.transport.http.HttpComponents5MessageSender
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+data class IpaasProperties(
+    val username: String,
+    val password: String,
+)
+
+fun newIpaasHttpClient(properties: IpaasProperties) = HttpClientBuilder.create()
+    .addRequestInterceptorFirst(HttpComponents5MessageSender.RemoveSoapHeadersInterceptor())
+    .addRequestInterceptorFirst(basicAuthInterceptor(properties.username, properties.password))
+    .setConnectionReuseStrategy(NoConnectionReuseStrategy.INSTANCE) // fix random "connection reset" errors
+    .build()
+
+fun dvvModificationRequestCustomizer(properties: IpaasProperties) = DvvModificationRequestCustomizer { request ->
+    request.authentication().basic(properties.username, properties.password)
+}
+
+private fun basicAuthInterceptor(username: String, password: String) = HttpRequestInterceptor { request, _, _ ->
+    request.addHeader(BasicHeader("Authorization", "Basic ${encode("$username:$password")}"))
+}
+
+private fun encode(data: String) = Base64.getEncoder().encodeToString(data.toByteArray(StandardCharsets.UTF_8))
+
+private class NoConnectionReuseStrategy : ConnectionReuseStrategy {
+    override fun keepAlive(request: HttpRequest?, response: HttpResponse?, context: HttpContext?): Boolean {
+        return false
+    }
+
+    companion object {
+        val INSTANCE = NoConnectionReuseStrategy()
+    }
+}

commit 85341cc9b7cec390cbfeb1b7ef8825409fa67692

Fix fuel manager bean
Regression from 49cf103965a6acc212b920874ae833a60b18b0ee

service/src/main/kotlin/fi/tampere/trevaka/person/config/PersonConfiguration.kt
@@ -34,6 +34,7 @@ class PersonConfiguration {
     /**
      * Custom [FuelManager] for [fi.espoo.evaka.dvv.DvvModificationsServiceClient].
      */
+    @Bean
     fun fuelManager() = FuelManager()
 
     @Bean