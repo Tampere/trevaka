d18e2c80fc0fd98a4eea99d02f627bc78b7d89e4
Jarkko Pätäri <jarkko.patari@gofore.com>
2023-12-19 09:05:19 +0200

    Refactor s3 client integration test init

--- service/src/test/kotlin/fi/tampere/trevaka/AbstractTampereIntegrationTest.kt
+++ service/src/test/kotlin/fi/tampere/trevaka/AbstractTampereIntegrationTest.kt
@@ -4,22 +4,8 @@
 
 package fi.tampere.trevaka
 
-import org.springframework.context.ApplicationListener
-import org.springframework.context.event.ContextRefreshedEvent
-import org.springframework.stereotype.Component
 import org.springframework.test.context.ActiveProfiles
-import software.amazon.awssdk.services.s3.S3Client
 import trevaka.AbstractIntegrationTest
 
 @ActiveProfiles(value = ["integration-test", "tampere_evaka"])
 abstract class AbstractTampereIntegrationTest : AbstractIntegrationTest()
-
-@Component
-class TampereIntegrationTestInit(private val client: S3Client, private val properties: TampereProperties) : ApplicationListener<ContextRefreshedEvent> {
-    override fun onApplicationEvent(event: ContextRefreshedEvent) {
-        val existingBuckets = client.listBuckets().buckets().map { it.name() }
-        properties.bucket.allBuckets()
-            .filterNot { bucket -> existingBuckets.contains(bucket) }
-            .forEach { bucket -> client.createBucket { it.bucket(bucket) } }
-    }
-}
