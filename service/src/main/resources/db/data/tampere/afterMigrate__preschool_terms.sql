b818ee7eb7ad713cdc2712d23a5488cfe00587ce
Jarkko Pätäri <jarkko.patari@gofore.com>
2024-01-18 12:21:15 +0200

    Remove obsoleted preschool terms delete

--- service/src/main/resources/db/data/tampere/afterMigrate__preschool_terms.sql
+++ service/src/main/resources/db/data/tampere/afterMigrate__preschool_terms.sql
@@ -2,9 +2,6 @@
 --
 -- SPDX-License-Identifier: LGPL-2.1-or-later
 
-DELETE FROM preschool_term WHERE id = '2d5aa324-1c73-11ed-bc5c-fbccc3898735';
-DELETE FROM preschool_term WHERE id = '5409a304-246a-11ed-b02b-fb2d9390be24';
-
 INSERT INTO preschool_term
     (id, finnish_preschool, swedish_preschool, extended_term, application_period, term_breaks)
 VALUES
