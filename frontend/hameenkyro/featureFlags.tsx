0e85ed50c09439168ef35c2fe260b33c38d11fd8
Santtu Nykänen <santtu.nykanen@gofore.com>
2024-02-19 11:45:06 +0200

    Enable preparatory education in Hämeenkyrö

--- frontend/hameenkyro/featureFlags.tsx
+++ frontend/hameenkyro/featureFlags.tsx
@@ -16,7 +16,7 @@ const featureFlags: FeatureFlags = {
   decisionDraftMultipleUnits: true,
   urgencyAttachments: false,
   preschool: true,
-  preparatory: false,
+  preparatory: true,
   assistanceActionOther: false,
   financeDecisionHandlerSelect: true,
   feeDecisionPreschoolClubFilter: true,
