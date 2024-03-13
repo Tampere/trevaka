d0434101e0b3ff6d9f9baf8345525f4ac6043971
Jarkko Pätäri <jarkko.patari@gofore.com>
2024-03-13 15:14:15 +0200

    Update invoice filename

--- service/src/main/kotlin/fi/vesilahti/evaka/invoice/service/S3Sender.kt
+++ service/src/main/kotlin/fi/vesilahti/evaka/invoice/service/S3Sender.kt
@@ -16,7 +16,7 @@ import java.util.*
 class S3Sender(private val s3Client: S3Client, private val properties: VesilahtiProperties) {
     fun send(content: String) {
         val bucket = properties.bucket.export
-        val fileName = SimpleDateFormat("'invoices/vesilahti-'yyyyMMdd-hhmmss'.txt'").format(Date())
+        val fileName = SimpleDateFormat("'invoices/Vesilahti_eVaka_'ddMMyyyy_hhmmss'.dat'").format(Date())
 
         val request = PutObjectRequest.builder()
             .bucket(bucket)
