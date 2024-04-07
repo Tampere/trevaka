2476ab25c1c97b7ac4f81fd599e3604f0b3be881
Santtu Nykänen <santtu.nykanen@gofore.com>
2024-04-07 18:50:49 +0200

    Add pirkkala invoicing configuration

---
+++ service/src/main/kotlin/fi/pirkkala/evaka/invoice/service/S3Sender.kt
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2024 Tampere region
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.pirkkala.evaka.invoice.service
+
+import fi.pirkkala.evaka.PirkkalaProperties
+import org.springframework.stereotype.Service
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import java.nio.charset.StandardCharsets
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Service
+class S3Sender(private val s3Client: S3Client, private val properties: PirkkalaProperties) {
+    fun send(content: String) {
+        val bucket = properties.bucket.export
+        val fileName = SimpleDateFormat("'invoices/Pirkkala_eVaka_'ddMMyyyy_HHmmss'.dat'").format(Date())
+
+        val request = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .contentType("text/csv")
+            .build()
+        val body = RequestBody.fromString(content, StandardCharsets.ISO_8859_1)
+        s3Client.putObject(request, body)
+    }
+}
