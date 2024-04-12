d06571f8028bc8e187ae1e42b5d6686e5e729c62
Jarkko Pätäri <jarkko.patari@gofore.com>
2024-04-12 10:26:39 +0300

    Fix placement types and update service need options

--- service/src/main/resources/db/data/lempaala/afterMigrate__service_need_options.sql
+++ service/src/main/resources/db/data/lempaala/afterMigrate__service_need_options.sql
@@ -2,6 +2,26 @@
 --
 -- SPDX-License-Identifier: LGPL-2.1-or-later
 
+DELETE
+FROM service_need_option_voucher_value
+WHERE id = '662570db-c149-45b9-a52f-2748b8995e19';
+DELETE
+FROM service_need_option
+WHERE id IN (
+             'acfb9f35-efff-40d9-8838-4a021cfc7446',
+             'a6dcce89-632c-4bb3-a3ba-1b3c6b570348',
+             'b0f13ba8-ff8a-4bb8-88b7-b260aefe8e0e',
+             'd5a5824b-ae3f-4cd3-9cd6-affee036bd1d',
+             '8d155bac-8be4-4988-9835-b99789ba51e4',
+             'e067df64-7ff0-42c1-a409-537db7c202dd',
+             '347a19fc-26a3-44c2-85a5-37ac8a56ae57',
+             '5a574d58-7d6f-4eb3-b91f-23ea7094e0de',
+             'f04ff070-35d7-4e2f-82ba-f130ef150c6d',
+             'c8e7bb24-584b-4624-865b-0cab145edfab',
+             'cf8b946d-45e6-4049-9439-ad0814f22592',
+             '131d5fbe-a72d-4f6e-82c0-5df88c657690'
+    );
+
 INSERT INTO service_need_option
     (id, name_fi, name_sv, name_en, valid_placement_type, default_option, fee_coefficient, occupancy_coefficient, occupancy_coefficient_under_3y, realized_occupancy_coefficient, realized_occupancy_coefficient_under_3y, daycare_hours_per_week, contract_days_per_month, daycare_hours_per_month, part_day, part_week, fee_description_fi, fee_description_sv, voucher_value_description_fi, voucher_value_description_sv, active, show_for_citizen, display_order)
 VALUES
@@ -10,19 +30,7 @@ VALUES
     ('f24a1b37-0be9-4004-8a00-eefda8ed925a', 'Varhaiskasvatus 86–120 tuntia / kuukausi', 'Varhaiskasvatus 86–120 tuntia / kuukausi', 'Daycare - 86-120 h per month', 'DAYCARE', FALSE, 0.8, 1.0, 1.75, 1.0, 1.75, 40, NULL, 120, TRUE, TRUE, 'Varhaiskasvatus 86–120 tuntia / kuukausi', 'Varhaiskasvatus 86–120 tuntia / kuukausi', 'Varhaiskasvatus 86–120 tuntia / kuukausi', 'Varhaiskasvatus 86–120 tuntia / kuukausi', TRUE, TRUE, 1),
     ('a92cf108-0939-45f5-8976-ab31e456b84d', 'Varhaiskasvatus 121-150 tuntia / kuukausi', 'Varhaiskasvatus 121-150 tuntia / kuukausi', 'Daycare - 121-150 h per month', 'DAYCARE', FALSE, 0.9, 1.0, 1.75, 1.0, 1.75, 40, NULL, 150, TRUE, TRUE, 'Varhaiskasvatus 121-150 tuntia / kuukausi', 'Varhaiskasvatus 121-150 tuntia / kuukausi', 'Varhaiskasvatus 121-150 tuntia / kuukausi', 'Varhaiskasvatus 121-150 tuntia / kuukausi', TRUE, TRUE, 2),
     ('22adbfd9-50d8-4192-9a35-1c5ce872e1a7', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Daycare - over 151 h per month', 'DAYCARE', FALSE, 1.0, 1.0, 1.75, 1.0, 1.75, 40, NULL, 210, FALSE, FALSE, 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', TRUE, TRUE, 3),
-    ('acfb9f35-efff-40d9-8838-4a021cfc7446', 'Palveluseteli Varhaiskasvatus 85 tuntia / kuukausi', 'Palveluseteli Varhaiskasvatus 85 tuntia / kuukausi', 'Voucher value Daycare - 85 h per month', 'DAYCARE', FALSE, 0.5, 1.0, 1.75, 1.0, 1.75, 40, NULL, 85, TRUE, TRUE, 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', TRUE, TRUE, 4),
-    ('a6dcce89-632c-4bb3-a3ba-1b3c6b570348', 'Palveluseteli Varhaiskasvatus 86–139 tuntia / kuukausi', 'Palveluseteli Varhaiskasvatus 86–139 tuntia / kuukausi', 'Voucher value Daycare - 86-139 h per month', 'DAYCARE', FALSE, 0.8, 1.0, 1.75, 1.0, 1.75, 40, NULL, 139, TRUE, TRUE, 'Varhaiskasvatus 86–139 tuntia / kuukausi', 'Varhaiskasvatus 86–139 tuntia / kuukausi', 'Varhaiskasvatus 86–139 tuntia / kuukausi', 'Varhaiskasvatus 86–139 tuntia / kuukausi', TRUE, TRUE, 5),
-    ('b0f13ba8-ff8a-4bb8-88b7-b260aefe8e0e', 'Palveluseteli Varhaiskasvatus yli 140 tuntia / kuukausi', 'Palveluseteli Varhaiskasvatus yli 140 tuntia / kuukausi', 'Voucher value Daycare - over 140 h per month', 'DAYCARE', FALSE, 1.0, 1.0, 1.75, 1.0, 1.75, 40, NULL, 210, FALSE, FALSE, 'Varhaiskasvatus yli 140 tuntia / kuukausi', 'Varhaiskasvatus yli 140 tuntia / kuukausi', 'Varhaiskasvatus yli 140 tuntia / kuukausi', 'Varhaiskasvatus yli 140 tuntia / kuukausi', TRUE, TRUE, 6),
-    ('d5a5824b-ae3f-4cd3-9cd6-affee036bd1d', 'Palveluseteli pph Varhaiskasvatus 85 tuntia / kuukausi', 'Palveluseteli pph Varhaiskasvatus 85 tuntia / kuukausi', 'Voucher value cm Daycare - 85 h per month', 'DAYCARE', FALSE, 0.5, 1.0, 1.75, 1.0, 1.75, 40, NULL, 85, TRUE, TRUE, 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', 'Varhaiskasvatus 85 tuntia / kuukausi', TRUE, TRUE, 7),
-    ('8d155bac-8be4-4988-9835-b99789ba51e4', 'Palveluseteli pph Varhaiskasvatus yli 151 tuntia / kuukausi', 'Palveluseteli pph Varhaiskasvatus yli 151 tuntia / kuukausi', 'Voucher value cm Daycare - over 151 h per month', 'DAYCARE', FALSE, 1.0, 1.0, 1.75, 1.0, 1.75, 40, NULL, 210, FALSE, FALSE, 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', 'Varhaiskasvatus yli 151 tuntia / kuukausi', TRUE, TRUE, 8),
-    ('e945728f-2671-4158-8941-1d39a3a36dce', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Temporary daycare', 'TEMPORARY_DAYCARE', TRUE, 1.0, 1.0, 1.75, 1.0, 1.75, 40, NULL, NULL, FALSE, FALSE, 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', TRUE, TRUE, NULL),
-    ('e067df64-7ff0-42c1-a409-537db7c202dd', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Preschool daycare - 0–50 h per month', 'PRESCHOOL_DAYCARE_ONLY', TRUE, 0.3, 1.0, 1.0, 1.0, 1.0, 40, NULL, 50, TRUE, TRUE, 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', TRUE, TRUE, NULL),
-    ('347a19fc-26a3-44c2-85a5-37ac8a56ae57', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Preschool daycare - 0–50 h per month', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 0.3, 1.0, 1.0, 1.0, 1.0, 40, NULL, 50, TRUE, TRUE, 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 0–50 tuntia / kuukausi', TRUE, TRUE, 4),
-    ('5a574d58-7d6f-4eb3-b91f-23ea7094e0de', 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', 'Preschool daycare – 51-85 h per month', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 0.5, 1.0, 1.0, 1.0, 1.0, 40, NULL, 85, TRUE, TRUE, 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 51–85 tuntia / kuukausi', TRUE, TRUE, 5),
-    ('f04ff070-35d7-4e2f-82ba-f130ef150c6d', 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', 'Preschool daycare – 68-120 h per month', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 0.8, 1.0, 1.0, 1.0, 1.0, 40, NULL, 120, TRUE, TRUE, 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 86–120 tuntia / kuukausi', TRUE, TRUE, 6),
-    ('c8e7bb24-584b-4624-865b-0cab145edfab', 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', 'Preschool daycare – 121-150 h per month', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 0.9, 1.0, 1.0, 1.0, 1.0, 40, NULL, 150, TRUE, TRUE, 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', 'Täydentävä varhaiskasvatus 121–150 tuntia / kuukausi', TRUE, TRUE, 7),
-    ('cf8b946d-45e6-4049-9439-ad0814f22592', 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', 'Preschool daycare – over 151 h per month', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 1.0, 1.0, 1.0, 1.0, 1.0, 40, NULL, 210, FALSE, FALSE, 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', 'Täydentävä varhaiskasvatus yli 151 tuntia / kuukausi', TRUE, TRUE, 8),
-    ('131d5fbe-a72d-4f6e-82c0-5df88c657690', 'Palveluseteli Täydentävä varhaiskasvatus', 'Palveluseteli Täydentävä varhaiskasvatus', 'Voucher value Preschool daycare', 'PRESCHOOL_DAYCARE_ONLY', FALSE, 0.5, 1.0, 1.0, 1.0, 1.0, 40, NULL, 210, FALSE, FALSE, 'Täydentävä varhaiskasvatus', 'Täydentävä varhaiskasvatus', 'Täydentävä varhaiskasvatus', 'Täydentävä varhaiskasvatus', TRUE, TRUE, 9)
+    ('e945728f-2671-4158-8941-1d39a3a36dce', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Temporary daycare', 'TEMPORARY_DAYCARE', TRUE, 1.0, 1.0, 1.75, 1.0, 1.75, 40, NULL, NULL, FALSE, FALSE, 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', 'Tilapäinen varhaiskasvatus', TRUE, TRUE, NULL)
 ON CONFLICT (id) DO
 UPDATE SET
     name_fi = EXCLUDED.name_fi,
@@ -75,12 +83,11 @@ INSERT INTO service_need_option_voucher_value
 (id, service_need_option_id, validity, base_value, coefficient, value, base_value_under_3y, coefficient_under_3y, value_under_3y)
 VALUES
     -- 2023-08-01 -
-    ('94347253-ba0a-472f-a33a-a628ce5d727c', 'acfb9f35-efff-40d9-8838-4a021cfc7446', daterange('2023-08-01', NULL, '[]'), 90100, 0.60, 54060, 140600, 0.60, 84330),
-    ('abb09e4d-317b-426a-ae0f-c906e2c0dafb', 'a6dcce89-632c-4bb3-a3ba-1b3c6b570348', daterange('2023-08-01', NULL, '[]'), 90100, 0.80, 72080, 140600, 0.80, 112480),
-    ('87c4588b-e786-433e-9a51-27213d193e7b', 'b0f13ba8-ff8a-4bb8-88b7-b260aefe8e0e', daterange('2023-08-01', NULL, '[]'), 90100, 1.00, 90100, 140600, 1.00, 140600),
-    ('85772d47-a7ef-4dd8-ad69-10b1353227aa', 'd5a5824b-ae3f-4cd3-9cd6-affee036bd1d', daterange('2023-08-01', NULL, '[]'), 78300, 0.50, 39150, 78300, 0.50, 39150),
-    ('2343782f-25f3-49ca-b0fb-e6f2b50ecc0c', '8d155bac-8be4-4988-9835-b99789ba51e4', daterange('2023-08-01', NULL, '[]'), 78300, 1.00, 78300, 78300, 1.00, 78300),
-    ('662570db-c149-45b9-a52f-2748b8995e19', '131d5fbe-a72d-4f6e-82c0-5df88c657690', daterange('2023-08-01', NULL, '[]'), 90100, 0.50, 45050, 140600, 0.50, 70300)
+    ('94347253-ba0a-472f-a33a-a628ce5d727c', '4b07f3ec-503c-4493-bef2-c5be84fc92b3', daterange('2023-08-01', NULL, '[]'), 90100, 0.50, 45050, 140600, 0.50, 70300),
+    ('abb09e4d-317b-426a-ae0f-c906e2c0dafb', '489dcf01-e11a-4ab8-8c36-1e672581eb6d', daterange('2023-08-01', NULL, '[]'), 90100, 0.50, 45050, 140600, 0.50, 70300),
+    ('87c4588b-e786-433e-9a51-27213d193e7b', 'f24a1b37-0be9-4004-8a00-eefda8ed925a', daterange('2023-08-01', NULL, '[]'), 90100, 0.80, 72080, 140600, 0.80, 112480),
+    ('85772d47-a7ef-4dd8-ad69-10b1353227aa', 'a92cf108-0939-45f5-8976-ab31e456b84d', daterange('2023-08-01', NULL, '[]'), 90100, 1.00, 90100, 140600, 1.00, 140600),
+    ('2343782f-25f3-49ca-b0fb-e6f2b50ecc0c', '22adbfd9-50d8-4192-9a35-1c5ce872e1a7', daterange('2023-08-01', NULL, '[]'), 90100, 1.00, 90100, 140600, 1.00, 140600)
 ON CONFLICT (id) DO
     UPDATE SET
                service_need_option_id = EXCLUDED.service_need_option_id,
