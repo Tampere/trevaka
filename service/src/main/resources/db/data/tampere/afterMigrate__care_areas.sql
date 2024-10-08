6d3557326329b5fea09d399c27e0beb68bfd884e
Tatu Repo <tatu.repo@gofore.com>
2024-10-08 15:19:21 +0300

    Restore area codes to fix invoice report area distribution

--- service/src/main/resources/db/data/tampere/afterMigrate__care_areas.sql
+++ service/src/main/resources/db/data/tampere/afterMigrate__care_areas.sql
@@ -5,9 +5,9 @@
 INSERT INTO care_area
     (id, name, area_code, sub_cost_center, short_name)
 VALUES
-    ('6529e31e-9777-11eb-ba88-33a923255570', 'Etelä', NULL, NULL, 'etela'),
-    ('6529f5a2-9777-11eb-ba89-cfcda122ed3b', 'Itä', NULL, NULL, 'ita'),
-    ('6529f6ce-9777-11eb-ba8a-8f6495ec5104', 'Länsi', NULL, NULL, 'lansi')
+    ('6529e31e-9777-11eb-ba88-33a923255570', 'Etelä', 1, NULL, 'etela'),
+    ('6529f5a2-9777-11eb-ba89-cfcda122ed3b', 'Itä', 2, NULL, 'ita'),
+    ('6529f6ce-9777-11eb-ba8a-8f6495ec5104', 'Länsi', 3, NULL, 'lansi')
 ON CONFLICT (id) DO
 UPDATE SET
     name = EXCLUDED.name,
