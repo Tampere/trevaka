bb8267cb985597b2f24f660d28d681d977e77b85
Jarkko Pätäri <jarkko.patari@gofore.com>
2025-01-09 20:16:44 +0200

    Upgrade to terraform 1.9.8

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 terraform {
-  required_version = ">= 1.9.5"
+  required_version = ">= 1.9.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
