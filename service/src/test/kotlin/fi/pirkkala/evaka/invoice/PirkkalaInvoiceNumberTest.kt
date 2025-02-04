b50cc7a5e5c4eb0c29b3f4738bd8e8e9b220e77f
Jarkko Pätäri <jarkko.patari@gofore.com>
2025-02-04 12:36:31 +0200

    Update next invoice number

--- service/src/test/kotlin/fi/pirkkala/evaka/invoice/PirkkalaInvoiceNumberTest.kt
+++ service/src/test/kotlin/fi/pirkkala/evaka/invoice/PirkkalaInvoiceNumberTest.kt
@@ -54,7 +54,7 @@ class PirkkalaInvoiceNumberTest : AbstractPirkkalaIntegrationTest() {
             tx.getInvoicesByIds(invoiceIds)
         }
 
-        assertThat(invoices).extracting<Long?> { it.number }.containsExactly(53000001)
+        assertThat(invoices).extracting<Long?> { it.number }.containsExactly(53089798)
     }
 
     @Test
@@ -66,7 +66,7 @@ class PirkkalaInvoiceNumberTest : AbstractPirkkalaIntegrationTest() {
             tx.insert(
                 DevInvoice(
                     status = InvoiceStatus.SENT,
-                    number = 53000001, // current series start
+                    number = 53089798, // current series start
                     invoiceDate = LocalDate.of(2024, 12, 9),
                     dueDate = LocalDate.of(2024, 12, 23),
                     periodStart = LocalDate.of(2024, 11, 1),
@@ -95,6 +95,47 @@ class PirkkalaInvoiceNumberTest : AbstractPirkkalaIntegrationTest() {
             tx.getInvoicesByIds(invoiceIds)
         }
 
-        assertThat(invoices).extracting<Long?> { it.number }.containsExactly(53000002)
+        assertThat(invoices).extracting<Long?> { it.number }.containsExactly(53089799)
+    }
+
+    @Test
+    fun `first invoice with second series start has correct number`() {
+        val invoices = db.transaction { tx ->
+            val employee = DevEmployee(roles = setOf(UserRole.FINANCE_ADMIN)).also { tx.insert(it) }
+            val clock = MockEvakaClock(HelsinkiDateTime.of(LocalDate.of(2025, 1, 7), LocalTime.of(12, 34)))
+            val headOfFamilyId = tx.insert(DevPerson(), DevPersonType.ADULT)
+            tx.insert(
+                DevInvoice(
+                    status = InvoiceStatus.SENT,
+                    number = 1, // first series start
+                    invoiceDate = LocalDate.of(2024, 11, 11),
+                    dueDate = LocalDate.of(2024, 11, 25),
+                    periodStart = LocalDate.of(2024, 10, 1),
+                    periodEnd = LocalDate.of(2024, 10, 31),
+                    headOfFamilyId = headOfFamilyId,
+                    areaId = AreaId(UUID.fromString("951ef865-6c59-4225-9b4c-28e2317dbcc0")),
+                ),
+            )
+            val draftInvoice1 = tx.insert(
+                DevInvoice(
+                    status = InvoiceStatus.DRAFT,
+                    number = null,
+                    invoiceDate = LocalDate.EPOCH,
+                    dueDate = LocalDate.EPOCH,
+                    periodStart = LocalDate.of(2024, 12, 1),
+                    periodEnd = LocalDate.of(2024, 12, 31),
+                    headOfFamilyId = headOfFamilyId,
+                    areaId = AreaId(UUID.fromString("951ef865-6c59-4225-9b4c-28e2317dbcc0")),
+                ),
+            )
+            val invoiceIds = listOf(draftInvoice1)
+            val invoiceDate = LocalDate.of(2025, 1, 7)
+            val dueDate = invoiceDate.plusWeeks(2)
+
+            invoiceService.sendInvoices(tx, employee.evakaUserId, clock.now(), invoiceIds, invoiceDate, dueDate)
+            tx.getInvoicesByIds(invoiceIds)
+        }
+
+        assertThat(invoices).extracting<Long?> { it.number }.containsExactly(53089798)
     }
 }
