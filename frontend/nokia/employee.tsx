9b226ed6ad2a3db6d675530a088e296c27f00825
Jarkko Pätäri <jarkko.patari@gofore.com>
2025-10-21 10:39:43 +0300

    Update placement tool texts

--- frontend/nokia/employee.tsx
+++ frontend/nokia/employee.tsx
@@ -21,6 +21,9 @@ const customizations: EmployeeCustomizations = {
   appConfig: {},
   translations: {
     fi: {
+      titles: {
+        placementTool: 'Esiopetussijoitusten tuonti'
+      },
       application: {
         serviceNeed: {
           connectedLabel: 'Täydentävä toiminta',
@@ -577,6 +580,11 @@ const customizations: EmployeeCustomizations = {
       terms: {
         extendedTermStartInfo:
           'Aika, jolloin varhaiskasvatusmaksu määräytyy täydentävän varhaiskasvatuksen mukaan.'
+      },
+      placementTool: {
+        title: 'Esiopetussijoitusten tuonti',
+        description:
+          'Voit luoda tuotetuista sijoitusehdotuksista hakemukset eVakaan. Hakemukset luodaan suoraan odottamaan päätöstä.'
       }
     },
     sv: {}
