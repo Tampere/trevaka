867bbd7712e38241830fd8f267413203d162d0ad
Jarkko Pätäri <jarkko.patari@gofore.com>
2025-10-24 12:28:06 +0300

    Remove s3 usage from maintenance mode

--- infra/deploy/maintenance-mode.tf
+++ infra/deploy/maintenance-mode.tf
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 resource "aws_lb_listener_rule" "maintenance_mode" {
-  count = var.maintenance_mode_bucket != null ? 1 : 0
+  count = var.maintenance_mode_enabled ? 1 : 0
 
   listener_arn = data.terraform_remote_state.base.outputs.public_alb_listener_arn
   priority     = 99
@@ -19,7 +19,7 @@ resource "aws_lb_listener_rule" "maintenance_mode" {
 
     fixed_response {
       content_type = "text/html"
-      message_body = "<html><body style=\"margin: 0;\"><iframe style=\"min-height: 100vh; min-width: 100vw;border: 0;\" src=\"//${var.maintenance_mode_bucket}.s3.amazonaws.com/maintenance-page/index.html\"/></body></html>"
+      message_body = "<html><body>Huoltokatko</body></html>"
       status_code  = "503"
     }
   }
